3324e5e4456e7a599ec8ee166cd02b0dcd6fa0e7
Ehsan Vali <ehsanvgfl@hotmail.com>
2021-05-13 21:07:27 +0430

    blazor sample added

---
+++ SimpleWPF/SimpleApplication.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SimpleApplication.Web.Global" Language="C#" %>
---
+++ SimpleWPF/SimpleApplication.Web/Global.asax.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace SimpleApplication.Web
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+#if DEBUG
+            var Cors = System.Configuration.ConfigurationManager.AppSettings["Cors"];
+            if (!string.IsNullOrEmpty(Cors))
+            {
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", Cors);
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "*");
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Accept, " + FormsAuthentication.FormsCookieName);
+                if (HttpContext.Current.Request.HttpMethod == "OPTIONS")
+                {
+                    HttpContext.Current.Response.AddHeader("Access-Control-Max-Age", "1728000");
+                    HttpContext.Current.Response.End();
+                }
+            }
+#endif
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
